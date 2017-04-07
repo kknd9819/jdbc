180d5e94b3c4738085473b605bb95c07f5e86148
kknd9819 <kknd9819@qq.com>
2017-04-07 15:38:38 +0800

    Initial commit

---
+++ src/main/java/top/zz/config/context/ApplicationContext.java
@@ -0,0 +1,68 @@
+package top.zz.config.context;
+
+import cn.shengyuan.tools.util.SpringUtil;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.Properties;
+
+/**
+ * Created by X-man on 2017/3/30.
+ */
+@Configuration
+@Order(value = 0)
+public class ApplicationContext {
+
+
+    @Bean
+    public DruidDataSource druidDataSource(){
+
+    }
+
+    @Bean
+    public DefaultKaptcha defaultKaptcha(){
+        DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
+        Properties properties = new Properties();
+        properties.setProperty("kaptcha.border","yes");
+        properties.setProperty("kaptcha.border.color","221,221,221");
+        properties.setProperty("kaptcha.textproducer.font.color","0,0,0");
+        properties.setProperty("kaptcha.image.width","100");
+        properties.setProperty("kaptcha.textproducer.font.size","30");
+        properties.setProperty("kaptcha.image.height","38");
+        properties.setProperty("kaptcha.session.key","captchaCode");
+        properties.setProperty("kaptcha.noise.impl","com.google.code.kaptcha.impl.NoNoise");
+        properties.setProperty("kaptcha.textproducer.char.length","4");
+        properties.setProperty("kaptcha.textproducer.font.names","microsoft yahei,Arial");
+        properties.setProperty("kaptcha.background.clear.from","246,246,246");
+        properties.setProperty("kaptcha.word.impl","com.google.code.kaptcha.text.impl.MyWordRenderer");
+        Config config = new Config(properties);
+        defaultKaptcha.setConfig(config);
+        return defaultKaptcha;
+    }
+
+    @Bean
+    @Lazy(false)
+    public SpringUtil springUtil(){
+        SpringUtil springUtil = new SpringUtil();
+        return springUtil;
+    }
+
+    @Bean
+    public ThreadPoolTaskExecutor threadPoolTaskExecutor(){
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setCorePoolSize(5);
+        threadPoolTaskExecutor.setMaxPoolSize(50);
+        threadPoolTaskExecutor.setQueueCapacity(100);
+        threadPoolTaskExecutor.setKeepAliveSeconds(60);
+        return threadPoolTaskExecutor;
+    }
+
+
+
+}
