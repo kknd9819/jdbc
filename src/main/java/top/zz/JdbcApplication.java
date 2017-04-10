1cadef43a841a202756a6143de74023d4713e7e1
kknd9819 <kknd9819@qq.com>
2017-04-10 15:47:49 +0800

    1

--- src/main/java/top/zz/JdbcApplication.java
+++ src/main/java/top/zz/JdbcApplication.java
@@ -2,8 +2,10 @@ package top.zz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages ={"top.zz","cn.shengyuan"})
 public class JdbcApplication {
 
 	public static void main(String[] args) {
